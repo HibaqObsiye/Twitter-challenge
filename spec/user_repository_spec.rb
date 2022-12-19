c5c5664f093ecc970384eb97d33f9a6aa1d5e87c
hibaq obsiye <obsiyehibaq@gmail.com>
2022-12-19 19:23:03 +0000

    find user by their email

---
+++ spec/user_repository_spec.rb
@@ -0,0 +1,59 @@
+require '../lib/user.rb' 
+require '../lib/user_repository.rb'
+require '../spec/spec_helper.rb'
+require '../lib/database_connection.rb'
+
+def reset_tweets_table
+    seed_sql = File.read('../databases/tweets_seeds.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'twitter_test' })
+    connection.exec(seed_sql)
+end
+
+def reset_users_table
+    seed_sql = File.read('../databases/tweets_seeds.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'twitter_test' })
+    connection.exec(seed_sql)
+end
+
+describe UserRepository do
+    before(:each) do
+        reset_tweets_table
+        reset_users_table  
+    end
+
+    context "Given entries of users" do
+       it "list all users" do
+            user = UserRepository.new
+            all_users = user.all
+
+            expect(all_users.count).to eq(2)
+        end
+    end
+
+    context "searching for a user by id" do
+        it "returns the specific user information" do
+            user = UserRepository.new
+           
+            specific_user = user.find(2)
+           
+            expect(specific_user["id"]).to eq("2")
+            expect(specific_user["name"]).to eq("name2")
+            expect(specific_user["username"]).to eq("name234")
+            expect(specific_user["email"]).to eq("name2@email.com")
+            expect(specific_user["password"]).to eq("password2")
+        end
+    end
+
+    context "searching users by specific email" do
+        it "returns specific user detail" do
+            user = UserRepository.new
+            specific_user = user.find_by_email("name2@email.com")
+          
+            expect(specific_user["id"]).to eq("2")
+        end
+    end
+
+
+
+
+end
